964a22acc9cb447ec02d0fa2932274266a865da7
Ajith <39941665+ajithAI@users.noreply.github.com>
2024-09-27 14:20:22 +0530

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,5 +1,5 @@
 cp /home/user/Mixtral_Inference/run_bm.py /home/user/Mixtral_Inference/TensorRT-LLM/examples
-cp /home/user/Mixtral_Inference/copy.sh /home/user/Mixtral_Inference/TensorRT-LLM/examples
+cp /home/user/Mixtral_Inference/utils.py /home/user/Mixtral_Inference/TensorRT-LLM/examples
 for i in {1..8}
 do
 cat dataset_llama_1024_2048_len.txt >> dataset_llama_8192_2048_len.txt
