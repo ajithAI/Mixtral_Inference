aa2ac2105018fe212d3d7ec649c59803c9182424
Ajith <39941665+ajithAI@users.noreply.github.com>
2024-09-27 13:08:22 +0530

    Update docker_run_mixtral_inference.sh

--- docker_run_mixtral_inference.sh
+++ docker_run_mixtral_inference.sh
@@ -6,7 +6,7 @@ FILE=${FOLDER}/Mixtral_8x7B_v0.1_TRT_Batch_${1}_Input_${2}_Output_${3}_${6}
 turbostat -i 1 > ${FILE}_TURBOSTAT.txt &
 nvidia-smi dmon -s pucvt > ${FILE}_NVIDIASMI.txt &
 sleep 2s
-docker exec TensorRT_LLM_8xGPU_CUDA_12.6.0_TRT_LLM_0.13 bash /home/user/Mixtral_Inference/run_mixtral_inference.sh ${1} ${2} ${3} ${4} ${5} ${FILE}
+docker exec TensorRT_LLM_8xGPU_CUDA_12.6.0 bash /home/user/Mixtral_Inference/run_mixtral_inference.sh ${1} ${2} ${3} ${4} ${5} ${FILE}
 sleep 2s
 pkill -9 turbostat
 pkill -9 nvidia-smi
