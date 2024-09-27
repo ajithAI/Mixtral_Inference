36ddec740af0a0830e9b8e5fb079b1215c363206
Ajith <39941665+ajithAI@users.noreply.github.com>
2024-09-27 12:35:39 +0530

    Update run_mixtral_inference.sh

--- run_mixtral_inference.sh
+++ run_mixtral_inference.sh
@@ -7,6 +7,6 @@ EXT=$6
 
 export DIR=/home/user/Mixtral_Inference
 mpirun -n 8 --allow-run-as-root --bind-to numa --rank-by hwthread --report-bindings python3 ${DIR}/TensorRT-LLM/examples/run_bm.py \ 
-            --run_profiling --tokenizer_dir=/home/user/Mixtral-8x7B-v0.1 --input_file=/home/user/dataset_llama_8192_2048_len.txt \
+            --run_profiling --tokenizer_dir=/home/user/Mixtral-8x7B-v0.1 --input_file=/home/user/Mixtral_Inference/dataset_llama_8192_2048_len.txt \
             --engine_dir=$DIR/TRT_Engines/Mixtral_8x7B_v0.1_TRT_Engine_FP8_8xGPU_MaxBatch_8192_MaxSeqLen_4096_CUDA_12.6_TRT_LLM_0.13_TP_8  \ 
             --max_input_length=${ILEN} --max_output_len=${OLEN} --batch=${BATCH} --iterations=${ITER} --warmup=${WARM} 2>&1 | tee ${EXT}.txt
